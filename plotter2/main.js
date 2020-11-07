69cb9f127b9b1f46b6723c86c770b897c127a4df
Natalia S <71048085+Natalia1248@users.noreply.github.com>
2020-11-07 18:22:45 -0300

    Add files via upload

---
+++ plotter2/main.js
@@ -0,0 +1,84 @@
+let started=false;
+
+let a=0.5;
+let from=-a;
+let to=a;
+let x1, y1, x2, y2;
+
+let pos=0;
+let step=1;
+let img;
+
+function der(x){
+    if(x!=0)
+        return 500*(Math.sin(1/x)-(Math.cos(1/x)/x));
+    else
+        return 0;
+}
+
+function f(x){
+    let c=150;
+    if(x!=0)
+        return 500*x*Math.sin(1/x)+c;
+    else
+        return c;
+}
+
+function hordownscale(d){
+    return d*Math.abs(from-to)/800;
+}
+
+function horupscale(d){
+    return d*800/Math.abs(from-to);
+}
+
+function hortrans(x){
+    return x+from;
+}
+
+function verupscale(d){
+    return 2*d;
+}
+function verinvert(y){
+    return 600-y;
+}
+
+
+function setup(){
+    createCanvas(800,600);
+    background(200);
+    stroke (0);
+    for(pos=0; pos<width; pos+=step){
+        x1=pos;
+        y1=verinvert(verupscale(f(hortrans(hordownscale(x1)))));
+        x2=pos+step;
+        y2=verinvert(verupscale(f(hortrans(hordownscale(x2)))));
+        line (x1, y1, x2, y2);
+    }
+    img=get();
+    stroke(255,0,0);
+}
+
+
+function draw(){
+    image (img, 0,0)
+
+    let m= der(hortrans(hordownscale(mouseX)));
+    function tang(x){
+        let a = hortrans(hordownscale(mouseX));
+        let b = f(a);
+        return (m*(x-a))+b;
+    }
+    x1= -400;
+    y1= verinvert(verupscale(tang(hortrans(hordownscale(x1)))));
+    x2= 800;
+    y2= verinvert(verupscale(tang(hortrans(hordownscale(x2)))));
+
+    line(x1, y1, x2, y2);
+
+    console.log(m);
+
+    if(started)
+        setFrequency(map(m,-50000, 50000, 200, 20000));
+
+}
---
+++ plotter2/oscillator.js
@@ -0,0 +1,81 @@
+
+
+
+var oscillator, gainNode, audioCtx;
+
+function init() {
+
+  started=true;
+  // create web audio api context
+  var AudioContext = window.AudioContext || window.webkitAudioContext;
+  audioCtx = new AudioContext();
+
+  // create Oscillator and gain node
+  oscillator = audioCtx.createOscillator();
+  gainNode = audioCtx.createGain();
+
+  // connect oscillator to gain node to speakers
+
+
+  oscillator.connect(gainNode);
+  gainNode.connect(audioCtx.destination);
+
+  // create initial frequency and volumn values
+
+  //var maxVol = 0.02;
+
+  var initialFreq = 3000;
+  var initialVol = 0.08;
+
+  // set options for the oscillator
+
+
+  oscillator.detune.value = 100; // value in cents
+  oscillator.start(0);
+
+  oscillator.onended = function() {
+    console.log('Your tone has now stopped playing!');
+  };
+
+  gainNode.gain.value = initialVol;
+  gainNode.gain.minValue = 1;
+  gainNode.gain.maxValue = 0.0001;
+
+  /*
+
+  // mute button
+
+  var mute = document.querySelector('.mute');
+
+  mute.onclick = function() {
+    if(mute.getAttribute('data-muted') === 'false') {
+      gainNode.disconnect(audioCtx.destination);
+      mute.setAttribute('data-muted', 'true');
+      mute.innerHTML = "Unmute";
+    } else {
+      gainNode.connect(audioCtx.destination);
+      mute.setAttribute('data-muted', 'false');
+      mute.innerHTML = "Mute";
+    };
+  }
+
+  */
+
+    oscillator.frequency.value = initialFreq;
+    //gainNode.gain.value = (KeyY/HEIGHT) * maxVol;
+}
+
+function one(){
+    oscillator.frequency.value = 500;
+}
+function two(){
+    oscillator.frequency.value = 8000;
+}
+
+function mute(){
+    gainNode.disconnect(audioCtx.destination);
+}
+
+function setFrequency(f){
+    oscillator.frequency.value = f;
+}
